d13abfe19b64697c423ef805cc225d29109c6bae
Ori Arditi <ori.rdt@gmail.com>
2017-02-03 13:38:51 +0200

    firebase 3.x.x support

--- test/integration.js
+++ test/integration.js
@@ -4,35 +4,39 @@ import Firebase from 'firebase';
 import FirebaseServer from 'firebase-server';
 import fireMigrate from '../src'
 
-const url = 'ws://localhost.firebaseio.test:5000';
 let server;
+let databaseRef;
 
 describe('Fire Migrate', () => {
   beforeEach(done => {
     if (!server) {
       server = new FirebaseServer(5000, 'localhost.firebaseio.test');
+      databaseRef = Firebase.initializeApp({
+        apiKey: 'AnyKey',
+        databaseURL: 'ws://localhost.firebaseio.test:5000'
+      }).database().ref();
     }
     done();
   });
 
   afterEach(done => {
-    new Firebase(url).set(null).then(done);
+    databaseRef.set(null).then(done);
   });
 
   it('simple scripts', done => {
-    fireMigrate.migrate(url, [a,b], 'd').then(done);
+    fireMigrate.migrate(databaseRef, [a,b], 'd').then(done);
   });
 
   it('concurrent runs', done => {
     new Promise.all([
-      fireMigrate.migrate(url, [a,b], 'd'),
-      fireMigrate.migrate(url, [a,b], 'd')
+      fireMigrate.migrate(databaseRef, [a,b], 'd'),
+      fireMigrate.migrate(databaseRef, [a,b], 'd')
     ]).then(() => done());
   });
 
   it('consecutive runs', done => {
-    fireMigrate.migrate(url, [a], 'd').then(() => {
-      fireMigrate.migrate(url, [a,b], 'd').then(done);
+    fireMigrate.migrate(databaseRef, [a], 'd').then(() => {
+      fireMigrate.migrate(databaseRef, [a,b], 'd').then(done);
     });
   });
 
